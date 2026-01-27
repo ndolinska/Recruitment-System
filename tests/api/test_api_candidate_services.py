dfb4687dcbfd430583cb35e55030a1863f28c113
n.dolinska <n.dolinska.215@studms.ug.edu.pl>
2026-01-27 14:36:42 +0100

    Poprawki do testów api - usunięcie starych prefiksów

--- tests/api/test_api_candidate_services.py
+++ tests/api/test_api_candidate_services.py
@@ -24,11 +24,11 @@ def sample_candidate():
     }
 
 def test_create_candidate_and_check_duplicate(client, sample_candidate):
-    response = client.post("/api/candidates", json=sample_candidate)
+    response = client.post("/candidates", json=sample_candidate)
     assert response.status_code == 201
     assert response.json['message'] == "Candidate created"
     assert len(registry.candidates) == 1
-    try2 = client.post("/api/candidates", json=sample_candidate)
+    try2 = client.post("/candidates", json=sample_candidate)
     assert try2.status_code == 409
     assert try2.json['message'] == "Candidate with this email already exists"
 
@@ -40,44 +40,44 @@ def test_create_candidate_invalid_email(client):
         "experience": 5,
         "salary_expectations": 15000
     }
-    response = client.post("/api/candidates", json=invalid_candidate)
+    response = client.post("/candidates", json=invalid_candidate)
     assert response.status_code == 400
     assert response.json['message'] == "Invalid candidate data"
     assert len(registry.candidates) == 0
     
 def test_get_all_candidates(client, sample_candidate):
-    client.post("/api/candidates", json=sample_candidate)
-    response = client.get("/api/candidates")
+    client.post("/candidates", json=sample_candidate)
+    response = client.get("/candidates")
     assert response.status_code == 200
     assert len(response.json) == 1
     assert response.json[0]['email'] == sample_candidate['email']
 
 def test_update_candidate(client, sample_candidate):
-    client.post("/api/candidates", json=sample_candidate)
+    client.post("/candidates", json=sample_candidate)
     update_data = {
         "first_name": "Piotr",
         "last_name": "Andrzejewski",
         "salary_expectations": 20000,
         "experience": 10
     }
-    response = client.patch(f"/api/candidates/{sample_candidate['email']}", json=update_data)
+    response = client.patch(f"/candidates/{sample_candidate['email']}", json=update_data)
     
     assert response.status_code == 200
     assert response.json['message'] == "Candidate updated"
 
-    response = client.patch(f"/api/candidates/ghost@email.com", json=update_data)
+    response = client.patch(f"/candidates/ghost@email.com", json=update_data)
     assert response.status_code == 404
     assert response.json['message'] == "Candidate not found"
     
 
 def test_delete_candidate(client, sample_candidate):
-    client.post("/api/candidates", json=sample_candidate)
-    response = client.delete(f"/api/candidates/{sample_candidate['email']}")
+    client.post("/candidates", json=sample_candidate)
+    response = client.delete(f"/candidates/{sample_candidate['email']}")
     assert response.status_code == 200
     assert response.json['message'] == "Candidate deleted"
     assert len(registry.candidates) == 0
 
 def test_delete_candidate_nonexistant(client):
-    response = client.delete("/api/candidates/ghost@test.pl")
+    response = client.delete("/candidates/ghost@test.pl")
     assert response.status_code == 404
     assert response.json['message'] == "Candidate not found"
